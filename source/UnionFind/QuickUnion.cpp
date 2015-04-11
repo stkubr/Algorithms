eb09b84aad9526ad16e1889782c6f1cb90ddecbe
stkubr <stkubr@gmail.com>
2015-04-11 21:21:24 +0200

    add C_QuickUnion with findRoot() connection check

--- source/UnionFind/QuickUnion.cpp
+++ source/UnionFind/QuickUnion.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "QuickUnion.h"
+
+C_QuickUnion::C_QuickUnion(unsigned int numElements) : C_SlowUnion(numElements) {
+}
+
+C_QuickUnion::C_QuickUnion(std::vector<unsigned int> *_IDs_ptr) : C_SlowUnion(_IDs_ptr) {
+}
+
+unsigned int C_QuickUnion::findRoot(unsigned int inx) {
+    while (inx != IDs_raw_ptr->at(inx)) inx = IDs_raw_ptr->at(inx);
+    return inx;
+}
+
+bool C_QuickUnion::areConnected(unsigned int inx1, unsigned int inx2) {
+    return findRoot(inx1) == findRoot(inx2);
+}
+
+void C_QuickUnion::connect(unsigned int inx1, unsigned int inx2) {
+    unsigned int inx_root_1 = findRoot(inx1);
+    unsigned int inx_root_2 = findRoot(inx2);
+
+    IDs_raw_ptr->at(inx_root_1) = inx_root_2;
+}
--- source/UnionFind/QuickUnion.h
+++ source/UnionFind/QuickUnion.h
@@ -5,9 +5,21 @@
 #ifndef ALGORITHMS_QUICKUNION_H
 #define ALGORITHMS_QUICKUNION_H
 
+#include <vector>
+#include "SlowUnion.h"
 
-class QuickUnion {
+class C_QuickUnion: public C_SlowUnion{
+private:
+    unsigned int findRoot(unsigned int inx);
 
+public:
+    C_QuickUnion(unsigned int numElements);
+
+    C_QuickUnion(std::vector<unsigned int> * _IDs_ptr);
+
+    virtual bool areConnected(unsigned int inx1, unsigned int inx2);
+
+    virtual void connect(unsigned int inx1, unsigned int inx2);
 };
 
 
