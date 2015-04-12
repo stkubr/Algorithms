802a5168edfa6c10df41006bdcdd7667c921f6cc
stkubr <stkubr@gmail.com>
2015-04-12 02:48:12 +0200

    add client to test sorting algorithms

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
 #include "ClassClients/ClientUF.h"
+#include "ClassClients/ClientSort.h"
 
 using namespace std;
 
 int main() {
     cout << "Algorithms Library presentation" << endl;
-    cout << "QuickFind example:" << endl;
+
+ /*   cout << "UnionFind example:" << endl;
     C_ClientUF clientUF;
     clientUF.test();
+ */
 
+    C_ClientSort clientSort(10);
+    clientSort.show();
 }
