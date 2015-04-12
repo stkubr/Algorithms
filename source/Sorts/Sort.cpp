686d8e4c74382e5cda9b33b9c456a884fb671fcc
stkubr <stkubr@gmail.com>
2015-04-12 02:49:00 +0200

    add initial interface class C_Sort for all sorting engines

---
+++ source/Sorts/Sort.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stkubr on 12.04.15.
+//
+
+#include "Sort.h"
---
+++ source/Sorts/Sort.h
@@ -0,0 +1,17 @@
+//
+// Created by stkubr on 12.04.15.
+//
+
+#ifndef ALGORITHMS_SORT_H
+#define ALGORITHMS_SORT_H
+
+
+#include <vector>
+
+class C_Sort {
+public:
+    void sort(std::vector<double> & data) {}
+};
+
+
+#endif //ALGORITHMS_SORT_H
